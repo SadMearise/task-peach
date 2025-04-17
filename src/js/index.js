c8c00f40adde5932ed89196dfab59667a8455ce5
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2025-04-17 14:39:20 +0300

    fix: rename file path

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import "../styles/main.scss";
 import "./map-cities.js";
-import "./map-filter.js";
+import "./map-filter-menu.js";
 import "./map-dropdown-menu.js";
 import "./accordion.js";
 import "./slider.js";
